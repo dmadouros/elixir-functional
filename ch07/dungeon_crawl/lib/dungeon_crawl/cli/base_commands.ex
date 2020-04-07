88fcdf9d6f28c12a411f276694f8f8bcdfb08300
David Madouros <david@earthvectors.com>
2020-04-07 07:59:17 -0600

    ch07-try-throw-catch

--- ch07/dungeon_crawl/lib/dungeon_crawl/cli/base_commands.ex
+++ ch07/dungeon_crawl/lib/dungeon_crawl/cli/base_commands.ex
@@ -1,6 +1,8 @@
 defmodule DungeonCrawl.CLI.BaseCommands do
   alias Mix.Shell.IO, as: Shell
 
+  @invalid_option {:error, "Invalid option"}
+
   def display_options(options) do
     options
     |> Enum.with_index(1)
@@ -14,11 +16,6 @@ defmodule DungeonCrawl.CLI.BaseCommands do
   def generate_question(options) do
     options = Enum.join(1..Enum.count(options), ", ")
     "Which one? [#{options}]\n"
-  end
-
-  def parse_answer(answer) do
-    {option, _} = Integer.parse(answer)
-    option - 1
   end
 
   def ask_for_index(options) do
@@ -52,34 +49,33 @@ defmodule DungeonCrawl.CLI.BaseCommands do
       |> display_options
       |> generate_question
       |> Shell.prompt
-      |> parse_answer!
-      |> find_option_by_index!(options)
-    rescue
-      e in DungeonCrawl.CLI.InvalidOptionError ->
-        display_error(e)
+      |> parse_answer
+      |> find_option_by_index(options)
+    catch
+      {:error, message} ->
+        display_error(message)
         ask_for_option(options)
     end
   end
 
-  def display_error(e) do
+  def display_error(message) do
     Shell.cmd("clear")
-    Shell.error(e.message)
+    Shell.error(message)
     Shell.prompt("Press Enter to try again.")
     Shell.cmd("clear")
   end
 
-  def parse_answer!(answer) do
+  def parse_answer(answer) do
     case Integer.parse(answer) do
       :error ->
-        raise DungeonCrawl.CLI.InvalidOptionError
+        throw @invalid_option
 
       {option, _} ->
         option - 1
     end
   end
 
-  def find_option_by_index!(index, options) do
-    Enum.at(options, index) ||
-      raise DungeonCrawl.CLI.InvalidOptionError
+  def find_option_by_index(index, options) do
+    Enum.at(options, index) || throw @invalid_option
   end
 end
